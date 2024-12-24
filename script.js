8e5a562e8da6dd68ce54626faabfc17f64b9352d
Leonora Sherifi <58704979+leonorasherifi@users.noreply.github.com>
2024-12-24 20:24:37 +0100

    favicon problem

--- script.js
+++ script.js
@@ -31,6 +31,22 @@ newQuoteButton.addEventListener('click', displayRandomQuote);
 
 
 
+// RAIN EFFECT -------------------------------------------------------------------------------------------
+const createRain = () => {
+  const rainDrop = document.createElement('div');
+  rainDrop.className = 'rain';
+  rainDrop.style.left = `${Math.random() * window.innerWidth}px`; // Random horizontal position
+  rainDrop.style.animationDuration = `${Math.random() * 0.5 + 1}s`; // Randomize fall speed
+  document.body.appendChild(rainDrop);
+
+  // Remove the drop after its animation ends
+  setTimeout(() => rainDrop.remove(), 5000);
+};
+
+// Create rain at intervals
+setInterval(createRain, 50);
+
+
 
 // SOUND EFFECT (mute/unmute) ----------------------------------------------------------------------------
 audio.loop = true; // Enable looping
@@ -48,29 +64,3 @@ muteButton.addEventListener('click', () => {
     muteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M792-56 671-177q-25 16-53 27.5T560-131v-82q14-5 27.5-10t25.5-12L480-368v208L280-360H120v-240h128L56-792l56-56 736 736-56 56Zm-8-232-58-58q17-31 25.5-65t8.5-70q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 53-14.5 102T784-288ZM650-422l-90-90v-130q47 22 73.5 66t26.5 96q0 15-2.5 29.5T650-422ZM480-592 376-696l104-104v208Zm-80 238v-94l-72-72H200v80h114l86 86Zm-36-130Z"/></svg>';
   }
 });
-
-
-
-// DELETE
-document.getElementById("newQuoteButton").addEventListener("click", () => {
-  audioElement.play();
-});
-
-
-
-
-// RAIN EFFECT -------------------------------------------------------------------------------------------
-const createRain = () => {
-  const rainDrop = document.createElement('div');
-  rainDrop.className = 'rain';
-  rainDrop.style.left = `${Math.random() * window.innerWidth}px`; // Random horizontal position
-  rainDrop.style.animationDuration = `${Math.random() * 0.5 + 1}s`; // Randomize fall speed
-  document.body.appendChild(rainDrop);
-
-  // Remove the drop after its animation ends
-  setTimeout(() => rainDrop.remove(), 5000);
-};
-
-// Create rain at intervals
-setInterval(createRain, 50);
-
